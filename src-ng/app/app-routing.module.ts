9f16aaae422abc4efd1af857414e88b46ccc1417
freylint <katvance64@gmail.com>
2021-08-22 15:51:42 -0500

    Create basic app layout and routing.

--- src-ng/app/app-routing.module.ts
+++ src-ng/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Pages
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'editor', component: EditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src-ng/app/app.module.ts
+++ src-ng/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
