a0d1bbb766b329f9d1c51cf647749f8f12a1b4d1
freylint <katvance64@gmail.com>
2021-08-22 17:38:20 -0500

    Add material design dependency.

--- src-ng/app/app.module.ts
+++ src-ng/app/app.module.ts
@@ -1,22 +1,38 @@
+// Angular Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+// Material Design Modules
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+
+// Site Components
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ProjectComponents } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
     HomeComponent,
-    ToolbarComponent
+    ProjectComponents.components[0],
   ],
   imports: [
+    // Angular Moduels
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    // Material Design Modules
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
