7a6296d0e6e33cd5b3a1d9c2ef4b26fd3cf1f318
freylint <katvance64@gmail.com>
2021-08-22 16:53:27 -0500

    add basic menus to toolbar.

---
+++ src-ng/app/components/toolbar/menus/file/file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'toolbar-menu-file',
+  templateUrl: './file.component.html',
+  styleUrls: ['./file.component.sass']
+})
+export class FileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src-ng/app/components/toolbar/menus/mode/mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'toolbar-menu-mode',
+  templateUrl: './mode.component.html',
+  styleUrls: ['./mode.component.sass']
+})
+export class ModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
